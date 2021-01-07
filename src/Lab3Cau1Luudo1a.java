5fe2c524331e58db8875f4e459e89685e0037961
nguyen <nhathung@gmail.com>
2021-01-07 16:08:12 +0700

    So thuc

--- src/Lab3Cau1Luudo1a.java
+++ src/Lab3Cau1Luudo1a.java
@@ -1,6 +1,6 @@
 
 import java.util.Scanner;
-public class Lab3Cau1 {
+public class Lab3Cau1Luudo1a {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
          int a = sc.nextInt();
