223d08ceccb17d0c77516baf4dd648a13e827df9
nguyen <nhathung@gmail.com>
2021-01-07 14:45:21 +0700

    Code xử lý lưu đồ 1a

---
+++ src/Lab3Cau1.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+public class Lab3Cau1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+         int a = sc.nextInt();
+         int b = sc.nextInt();
+         int c = 2;
+      if(b==0)
+      {System.out.println("Error divide by zero"); }
+      else if(c == a / b) {System.out.println("c = " + (a/b));
+      }
+
+    }
+}
